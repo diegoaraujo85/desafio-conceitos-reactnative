e0b1e52793f5b8484040b5facf65e13a1f310dc1
Diego Araujo <doasalah@gmail.com>
2020-04-12 15:50:15 -0300

    desafio react native test fail

---
+++ src/components/Repository.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+// import { Container } from './styles';
+
+export default function components({ repository, handleLikeRepository }) {
+  return <></>;
+}
+
+const styles = StyleSheet.create({});
